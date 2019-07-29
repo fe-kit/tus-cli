74577ab2ecb6eadca39b2f6adc787834fe209350
www.wolong.cn <tjubao@gmail.com>
2019-07-29 14:37:06 +0800

    修改创建流程

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,17 @@
-## 1.0.1 (2019-07-10)
+# tus-cli
 
-#### 添加 Vue 模板生成
+# 1.0.1
+
+## 添加创建目录判断
+
+# 1.0.2
+
+## 添加 install 进度提示
+
+# 1.0.3
+
+## 添加 didyoumean、validate-npm-package-name
+
+## 添加 options 参数 -t -f 设置
+
+## 替换 fs 为 fs-extra
--- bin/cli.js
+++ bin/cli.js
@@ -1,21 +1,66 @@
 #!/usr/bin/env node
+
 const path = require('path');
 const program = require('commander');
 const utils = require('../utils');
+const chalk = require('chalk');
+const didYouMean = require('didyoumean');
 
 process.env.NODE_PATH = __dirname + '/../node_modules/';
-program.version(require('../package').version);
-program.usage('<command>');
+
+program.version(require('../package').version).usage('<command> [options]');
 
 program
-  .command('create')
-  .description('create a new project from template')
+  .command('create <app-name>')
+  .version('0.0.1', '-v, --version')
+  .description('create a new project powered by tus-cli')
+  .option('-t, --type <typeName>', 'The type of project to be created！')
+  .option('-f, --force', 'Overwrite target directory if it exists')
   .alias('c')
-  .action((name) => {
-    require('../lib/create')(name);
+  .action((projectName, cmd) => {
+    const options = cleanArgs(cmd);
+    require('../lib/create')(projectName, options);
   });
-program.parse(process.argv);
 
-if (!process.argv.length) {
-  program.help();
+// output help information on unknown commands
+program.arguments('<command>').action((cmd) => {
+  program.outputHelp();
+  console.log(`  ` + chalk.red(`Unknown command ${chalk.yellow(cmd)}.`));
+  console.log();
+  suggestCommands(cmd);
+});
+
+program.parse(process.argv);
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
+
+function suggestCommands(cmd) {
+  const availableCommands = program.commands.map((cmd) => {
+    return cmd._name;
+  });
+
+  const suggestion = didYouMean(cmd, availableCommands);
+  if (suggestion) {
+    console.log(`  ` + chalk.red(`Did you mean ${chalk.yellow(suggestion)}?`));
+  }
+}
+
+function camelize(str) {
+  return str.replace(/-(\w)/g, (_, c) => (c ? c.toUpperCase() : ''));
+}
+
+// commander passes the Command object itself as options,
+// extract only actual options into a fresh object.
+function cleanArgs(cmd) {
+  const args = {};
+  cmd.options.forEach((o) => {
+    const key = camelize(o.long.replace(/^--/, ''));
+    // if an option is not present and Command has a method with the same name
+    // it should not be copied
+    if (typeof cmd[key] !== 'function' && typeof cmd[key] !== 'undefined') {
+      args[key] = cmd[key];
+    }
+  });
+  return args;
 }
